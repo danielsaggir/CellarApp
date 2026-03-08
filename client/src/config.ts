1d7e9894993545c92b7f0fd9971751317858ae4e
danielsaggir <danielsaggir@gmail.com>
2026-03-08 18:17:29 +0200

    Update iOS project configuration and API URL for development
    - Removed unused input and output paths in the Xcode project file for better clarity.
    - Updated iOS API URL to a new local IP address for development purposes.

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 import { Platform } from "react-native";
 
 const API_URL = Platform.select({
-  ios: "http://192.168.1.117:4000",
+  ios: "http://192.168.1.124:4000",
   android: "http://10.0.2.2:4000",
   default: "http://localhost:4000",
 })!;
